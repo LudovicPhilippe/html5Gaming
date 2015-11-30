336be0cb59d23d070bd14eb52cba903ffee7c78c
Ludovic <ludovic.philippe32@gmail.com>
2015-11-30 20:18:32 +0100

    remoce unecessay file

--- db.js
+++
@@ -1,8 +0,0 @@
-
-// Inclusion de Mongoose
-var mongoose = require('mongoose');
-
-// connexion
-mongoose.connect('mongodb://localhost/html5Gaming', function(err) {
-    if (err) { throw err; }
-});
